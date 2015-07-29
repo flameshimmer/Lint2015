3305b24b6e84936017079c69bc517c516b6537cb
flameshimmer <nm1072@gmail.com>
2015-07-29 15:24:06 -0700

    C

--- Lint2015/CoinsInALineIII_Hard.cpp
+++ Lint2015/CoinsInALineIII_Hard.cpp
@@ -1,11 +1,46 @@
 #include "stdafx.h"
 
+//There are n coins in a line.Two players take turns to take a coin from one of the ends of the line until 
+//there are no more coins left.The player with the larger amount of money wins.
+//
+//Could you please decide the first player will win or lose ?
+//
+//Example
+//Given array A = [3, 2, 2], return true.
+//
+//Given array A = [1, 2, 4], return true.
+//
+//Given array A = [1, 20, 4], return false.
+//
+//Challenge
+//Follow Up Question :
+//
+//If n is even.Is there any hacky algorithm that can decide whether first player will win or lose in O(1) memory and O(n) time ?
+
 namespace LintSolution1
 {
     namespace CoinsInALineIII_Hard
     {
      
-     
+		bool firstWillWin(vector<int> &values) {
+			int len = values.size();
+			if (len == 0) { return false; }
+			if (len == 1) { return true; }
+
+			vector<vector<int>>M(len, vector<int>(len, 0));
+			int total = 0;
+			for (int i = len - 1; i >= 0; i--)
+			{
+				total += values[i];
+				for (int j = i; j < len; j++)
+				{
+					int val1 = min((i+2 < len ? M[i+2][j] : 0), ((i+1 < len && j-1 >=0) ? M[i+1][j-1] : 0)) + values[i];
+					int val2 = min((i+1 < len && j-1 >= 0) ? M[i+1][j-1] : 0, (j-2 >= 0 ? M[i][j-2] : 0)) + values[j];
+					M[i][j] = max(val1, val2);
+				}
+			}
+			return M[0][len - 1] > total / 2;
+		}
      
      
         void Main()
--- Lint2015/CoinsInALineII_Medium.cpp
+++ Lint2015/CoinsInALineII_Medium.cpp
@@ -13,43 +13,43 @@
 
 namespace LintSolution1
 {
-    namespace CoinsInALineII_Medium
-    {
+	namespace CoinsInALineII_Medium
+	{
 		/**
 		* @param values: a vector of integers
 		* @return: a boolean which equals to true if the first player will win
 		*/
 		bool firstWillWin(vector<int> &values) {
 			int len = values.size();
-			if (len == 0) { return false; }
-			reverse(values.begin(), values.end());
+			if (len < 3) { return true; }
 
-			int M[4];
-			M[0] = 0;
-			M[1] = values[0];
-			M[2] = values[0] + values[1];
-			M[3] = M[2];
-
-			int total = 0;
-			for (int i = 1; i < len+1; i++)
+			vector<int> M(len, 0);
+			M[len - 1] = values[len - 1];
+			M[len - 2] = values[len - 1] + values[len - 2];
+			int total = M[len - 2];
+			for (int i = len - 3; i >= 0; i--)
 			{
-				total += values[i-1];
-				if (i >= 4)
-				{
-					int maxV1 = max(M[(i - 2) % 4], M[(i - 3) % 4]) + values[i - 1];
-					int maxV2 = max(M[(i - 3) % 4], M[(i - 4) % 4]) + values[i - 1] + values[i-2];
-					M[i % 4] = max(maxV1, maxV2);
-				}
+				total += values[i];
+				int val1 = values[i] + min((i + 2 < len ? M[i + 2] : 0), (i + 3 < len ? M[i + 3] : 0));
+				int val2 = values[i] + values[i + 1] + min((i + 3 < len ? M[i + 3] : 0), (i + 4 < len ? M[i + 4] : 0));
+				M[i] = max(val1, val2);
 			}
-			return M[len % 4] > total / 2;
+			return M[0] > total / 2;
 		}
-     
-        void Main()
-        {
-			vector<int> input = createVector({1, 2, 4});
+
+
+		void Main()
+		{
+			vector<int> input;
+			input = createVector({ 1, 2, 2 });
 			print(firstWillWin(input));
-        }
-    }
+
+			input = createVector({ 1, 2, 4 });
+			print(firstWillWin(input));
+
+			input = createVector({ 100, 200, 400, 300, 400, 800, 500, 600, 1200 });
+			print(firstWillWin(input));
+		}
+	}
 }
-   
-    
+
